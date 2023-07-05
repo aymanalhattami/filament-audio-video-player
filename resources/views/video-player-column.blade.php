ad7de6799b864627fe83205b154ea977d68479e0
Ayman Alhattami <ayman.m.alhattami@gmail.com>
2023-07-05 22:53:05 +0300

    feat: create video player column for table

---
+++ resources/views/video-player-column.blade.php
@@ -0,0 +1,7 @@
+<video controls
+    class="js-player" width="9" height="16"
+    crossorigin
+    playsinline>
+    <source src="{{ $getState() }}"
+        type="video/mp4">
+</video>
---
+++ src/VideoPlayerColumn.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AymanAlhattami\FilamentAudioVideoPlayer;
+
+use Filament\Tables\Columns\Column;
+
+class VideoPlayerColumn extends Column
+{
+    protected string $view = 'filament-audio-video-player::video-player-column';
+}
